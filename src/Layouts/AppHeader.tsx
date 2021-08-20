145de002c019f1663911fb211e8afb059243cc06
vitalikvaraksa <84393466+vitalikvaraksa@users.noreply.github.com>
2021-08-20 23:33:35 +0300

    logo responsive

--- src/Layouts/AppHeader.tsx
+++ src/Layouts/AppHeader.tsx
@@ -10,18 +10,21 @@ import opusLogo from "../media/images/opus.png"
 const useStyles = makeStyles(({ constants, palette, zIndex }: ITheme) => {
   return createStyles({
     root: {
+      padding: "20px 160px",
       display: "flex",
       position: "fixed",
       justifyContent: "space-between",
-      padding: `${constants.generalUnit * 2}px ${constants.generalUnit * 4}px`,
+      //padding: `${constants.generalUnit * 2}px ${constants.generalUnit * 4}px`,
       width: "100%",
       top: 0,
       left: 0,
       backgroundColor: "rgb(26,26,28)",
-      borderBottom: `1px solid ${palette.additional["header"][3]}`,
       color: "white",
       alignItems: "center",
       zIndex: zIndex?.layer2,
+      '@media(max-width: 780px)' : {
+        padding: "20px 20px"
+      }
     },
     left: {
       display: "flex",
@@ -70,7 +73,7 @@ const AppHeader: React.FC<IAppHeader> = () => {
         {/* <div className={classes.logo}>
         
         </div> */}
-        <img  src={opusLogo} style={{width: "50px", height: "50px"}} alt="Opus Token"/>
+        <img  src={opusLogo} style={{width: "40px", height: "40px"}} alt="Opus Token"/>
         <Typography variant="h2">Bridge</Typography>
       </div>
       <section className={classes.state}>
